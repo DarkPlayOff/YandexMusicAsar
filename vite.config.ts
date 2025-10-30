fbbcb0891def16388be6914d371176bb68588e4a
Dark <131844488+DarkPlayOff@users.noreply.github.com>
2025-10-30 19:10:19 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
@@ -8,10 +7,9 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: env.VITE_BASE_URL ?? "/",
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     resolve: {
       alias: {
-        "@ui": path.resolve(__dirname, "./src/mod/features/ui"),
         "~": path.resolve(__dirname, "./src"),
       },
     },
