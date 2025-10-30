cd348b3348543435438cba736a02a33524ebd7d6
Dark <131844488+DarkPlayOff@users.noreply.github.com>
2025-10-30 19:14:07 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
+
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: env.VITE_BASE_URL ?? "/",
-    plugins: [react()],
+    plugins: [],
     resolve: {
       alias: {
         "~": path.resolve(__dirname, "./src"),
